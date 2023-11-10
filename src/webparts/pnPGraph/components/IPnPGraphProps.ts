5eee83af9d71f6d485cc8e299aec351bee539284
Daniel Merillas <dmerillasg@minsait.com>
2023-11-10 21:21:49 +0100

    first commit

---
+++ src/webparts/pnPGraph/components/IPnPGraphProps.ts
@@ -0,0 +1,7 @@
+export interface IPnPGraphProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/pnPGraph/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IPnPGraphWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'PnPGraphWebPartStrings' {
+  const strings: IPnPGraphWebPartStrings;
+  export = strings;
+}
